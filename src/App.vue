f3b43e0c6fc33facece49f25a203cfed6c204564
rajat tyagi <rajatmath.2013@gmail.com>
2020-02-06 18:02:01 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Wrapper 
+      v-for="(data,index) in mainData"
+      :key="data"
+      :title="data.index"
+      :actual-index="index"
+      v-on:add-new-wrapper="addNewWrapper" 
+      v-on:delete-wrapper="deleteWrapper" 
+      :main-data="data" :index="data.index"></Wrapper>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Wrapper from './components/wrapper.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Wrapper
+  },
+  data() {
+    return {
+      mainData:[]
+    }
+  },
+  mounted(){
+    this.init();
+  },
+  methods:{
+    title(){
+      return Math.round(((Math.random()*100)%100));
+    },
+    init() {
+      this.mainData.push({
+        index: this.title()
+      });
+    },
+    deleteWrapper({index}){
+      this.mainData = this.mainData.filter(data => {
+        if(data.index == index){
+          return false;
+        } return true;
+      })
+      // let newData=[];
+      // window.console.log("-->>",this.mainData);
+      // let count = 1;
+      // for(let i in this.mainData){
+      //   window.console.log('-->>.',i);
+      //   if(this.mainData[i].index != index){
+      //     newData.push({
+      //       ...this.mainData[i],
+      //       index: count++
+      //     })
+      //   }
+      // }
+      // window.console.log("-->>>",newData);
+      // this.mainData = newData;
+    },
+    addNewWrapper({index}){
+      this.mainData = [...this.mainData.slice(0,index),{
+        index: this.title()
+      },...this.mainData.slice(index)]
+    },
   }
 }
 </script>
---
+++ src/components/wrapper.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="wrapper">
+        <div class="header">
+            <div class="title">{{ title }}</div>
+            <div @click="addNewWrapper()" class="plus">+</div>
+            <div @click="deleteWrapper()" class="cross">X</div>
+        </div>
+        <div class="content-wrapper">
+            <div class="content" v-for="data in mainData" :key="data">
+                <div class="text">{{ data }}</div>
+                <div class="addNewSubChild" @click="addNewSubChild()">+</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        mainData: {
+           
+        },
+        title:{
+        },
+        actualIndex:{
+            type:Number
+        }
+    },
+    data() {
+        return {
+            
+        }
+    },
+    methods:{
+        addNewWrapper(){
+            this.$emit("add-new-wrapper",{index: this.actualIndex});
+        },
+        deleteWrapper(){
+            this.$emit("delete-wrapper",{index: this.mainData.index});
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper{
+    border: 1px solid black;
+    .header{
+        display: flex;
+        justify-content: space-between;
+        background: white;
+        padding:1rem;
+        .plus{
+            padding: 1rem;
+            background: yellow;
+            cursor: pointer;
+        }
+    }
+    .content-wrapper{
+        display: flex;
+    }
+}
+</style>
+
+
